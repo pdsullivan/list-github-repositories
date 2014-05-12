75ff9e600457856ee607969e65d27542c1def82a
Patrick Sullivan <patdsully88@gmail.com>
2014-05-12 15:28:55 -0500

    Update user.js

--- javascripts/user.js
+++ javascripts/user.js
@@ -1,5 +1,10 @@
 function UserCtrl($scope) {
   
-  $scope.User = $http.get('https://api.github.com/users/pdsullivan');
- 
+  //$scope.User = $http.get('https://api.github.com/users/pdsullivan');
+  $http.get('https://api.github.com/users/pdsullivan').success(successCallback);
+  
+  function successCallback (data){
+    $scope.User = data;
+  };
+  
 }
