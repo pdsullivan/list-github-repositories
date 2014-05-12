18f76ae82e0d70541ccfb9b7d1e8f08156253c1e
Patrick Sullivan <patdsully88@gmail.com>
2014-05-12 15:43:10 -0500

    Update user.js

--- javascripts/user.js
+++ javascripts/user.js
@@ -1,14 +1,36 @@
-function UserCtrl($scope) {
-  
-  //$scope.User = $http.get('https://api.github.com/users/pdsullivan');
+(function () {
 
-  
-  $http({method: 'GET', url: 'https://api.github.com/users/pdsullivan'}).
-    success(function(data, status, headers, config) {
-      $scope.User = data;
-    }).
-    error(function(data, status, headers, config) {
-      alert('error');
-    });
-  
-}
+    angular.module("app", []);
+
+}());
+
+(function (app) {
+    var UserCtrl = function ($scope, linksService, $timeout) {
+        var onError = function () {
+            //alert("error!!");
+        };
+        var onLinks = function (response) {
+            $scope.user = response.data;
+        };
+
+        var init = function () {
+            $scope.links = linksService.getUser().then(onLinks, onError);
+        };
+        init();
+    };
+    app.controller("linksController", ["$scope", "linksService", "$timeout", linksController]);
+}(angular.module("appuserService
+
+
+(function (app) {
+    var baseUrl = " 'https://api.github.com'";
+    var linksService = function ($http) {
+        var linksFactory = {};
+        linksFactory.getUser = function () {
+            return $http.get(baseUrl + "/users/pdsullivan");    
+        };
+        return linksFactory;
+    };
+    app.factory("linksService", ["$http", linksService]);
+}(angular.module("app")));
+
